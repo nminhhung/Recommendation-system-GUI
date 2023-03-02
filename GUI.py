bea581d2c8a344bf67d5c59e35a5930d6ca062e7
Hung Nguyen Minh <47750189+nminhhung@users.noreply.github.com>
2023-03-03 00:54:04 +0700

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from streamlit_pandas_profiling import st_profile_report
+st.set_option('deprecation.showPyplotGlobalUse', False)
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
