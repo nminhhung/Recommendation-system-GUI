be9da7626c20957525098364459ec83ed3b30fde
Hung Nguyen Minh <47750189+nminhhung@users.noreply.github.com>
2023-03-03 09:45:35 +0700

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -200,8 +200,7 @@ if choice == 'Recommendation Systems':
             <p style='text-align: justify;'> 
             Overall, recommendation systems powered by machine learning represent a powerful tool for businesses seeking to improve customer engagement, drive sales, and gain valuable insights into the behavior of their target audience. As the technology continues to evolve, it is likely that these systems will become even more sophisticated and capable, providing even greater value to businesses of all sizes and industries.
             </p>
-            <p style='text-align: justify;'> 
-            :___________________________________________________________________________________________:
+            <p style='text-align: justify;'>             
             </p>
             """, unsafe_allow_html=True)
 
